8fd776701d739e898fd678ab75e4bbaa1bdeac7e
PEDRO HENRIQUE AMANCIO DOS SANTOS ROSA <p.rosa@labcetec.net>
2025-05-19 19:42:52 -0300

    Validador ta 90%

--- Validador_de_CPF.js
+++ Validador_de_CPF.js
@@ -1,24 +1,25 @@
 function validarCPF(cpf) {
+    // Verifica se contém '.' ou '-'
+    if (cpf.includes('.') || cpf.includes('-')) return false;
+
     cpf = cpf.replace(/[^\d]+/g, ''); // Remove caracteres não numéricos
     if (cpf.length !== 11 || /^(\d)\1+$/.test(cpf)) return false;
 
-    let soma = 0, resto;// cria um avariável para armazenar a soma e o resto da divisão
+    let soma = 0, resto;
 
     // Validação do primeiro dígito verificador
     for (let i = 1; i <= 9; i++) {
-        soma += parseInt(cpf.substring(i - 1, i)) * (11 - i);// multiplica o número pelo peso correspondente
-    // Exemplo: para o primeiro dígito, multiplica por 10, depois por 9, e assim por diante
+        soma += parseInt(cpf.substring(i - 1, i)) * (11 - i);
     }
     resto = (soma * 10) % 11;
-    if (resto === 10 || resto === 11) resto = 0; // transforma o resto em 0 se for 10 ou 11 para fazer a validação
-    if (resto !== parseInt(cpf.substring(9, 10))) return false;// verifica se o resto é igual ao dígito verificador
-    // Exemplo: se o CPF for 123.456.789-00, verifica se o resto é igual a 0 com base nas variaveis criadas,somoa e resto
+    if (resto === 10 || resto === 11) resto = 0;
+    if (resto !== parseInt(cpf.substring(9, 10))) return false;
 
-    soma = 0;// reinicia a soma para calcular o segundo dígito verificador
+    soma = 0;
 
     // Validação do segundo dígito verificador
     for (let i = 1; i <= 10; i++) {
-        soma += parseInt(cpf.substring(i - 1, i)) * (12 - i);// multiplica o número pelo peso correspondente segue o mesmo padrão da primeira validação, porem com outros numeros 
+        soma += parseInt(cpf.substring(i - 1, i)) * (12 - i);
     }
     resto = (soma * 10) % 11;
     if (resto === 10 || resto === 11) resto = 0;
